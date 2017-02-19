e6eff63ba33f19e3d1b01fc3915469f0d61d923f
Adrian <3144624477@qq.com>
2017-02-19 19:07:09 +0800

    create LoginServlet file

---
+++ src/com/lib/web/LoginServlet.java
@@ -0,0 +1,19 @@
+package com.lib.web;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet{
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        
+    }
+    
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+        this.doGet(request, response);
+    }
+}
